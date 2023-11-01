56708cfb5bb37a5083d663c4c5edd3c62faeecf8
SanyaW29 <SSWadhwa16@gmail.com>
2023-10-31 23:37:11 -0400

    Login and Registration Page

---
+++ mobile/Registration.js
@@ -0,0 +1,133 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+export default function RegistrationPage() {
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const navigation = useNavigation();
+
+  const handleRegister = () => {
+    // Your registration logic here
+    if (password === confirmPassword) {
+      // Successful registration action
+      console.log('Registration successful');
+    } else {
+      // Passwords do not match
+      console.log('Passwords do not match');
+    }
+  };
+
+  const navigateToLogin = () => {
+    navigation.navigate('Login'); // Navigate back to the Login screen
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Registration</Text>
+
+      <Text style={styles.label}>Email</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setEmail}
+        value={email}
+        placeholder="Enter your email"
+      />
+
+      <Text style={styles.label}>Phone Number</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPhoneNumber}
+        value={phoneNumber}
+        placeholder="Enter your phone number"
+      />
+
+      <Text style={styles.label}>Username</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setUsername}
+        value={username}
+        placeholder="Enter your username"
+      />
+
+      <Text style={styles.label}>Password</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+        placeholder="Enter your password"
+        secureTextEntry
+      />
+
+      <Text style={styles.label}>Confirm Password</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setConfirmPassword}
+        value={confirmPassword}
+        placeholder="Confirm your password"
+        secureTextEntry
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleRegister}>
+        <Text style={styles.buttonText}>Register</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.loginButton} onPress={navigateToLogin}>
+        <Text style={styles.loginText}>Already have an account? Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#23292D',
+    justifyContent: 'center',
+    padding: 16,
+  },
+  heading: {
+    color: '#FFFFFF',
+    fontSize: 45,
+    paddingBottom: 30,
+    textAlign: 'center',
+  },
+  label: {
+    fontSize: 16,
+    color: '#FFFFFF',
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    backgroundColor: '#FFFFFF',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 8,
+    borderRadius: 10,
+  },
+  button: {
+    backgroundColor: '#114E7A',
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 25,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  loginButton: {
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  loginText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    textDecorationLine: 'underline',
+  },
+});
